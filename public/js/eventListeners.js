e055ed892eb5d92a635ca1d637686280b10da1d9
7MIMIRA <7mimira@gmail.com>
2023-09-11 02:11:21 -0400

    Add event listener for submit button
    Just exploring how to capture the value of the text area element
    and having that tied to the click of the submit button

--- public/js/eventListeners.js
+++ public/js/eventListeners.js
@@ -1,3 +1,12 @@
 function initializeListeners() {
-    console.log('initialized listeners yo');
+    intializeSubmitButton();
 };
+
+function intializeSubmitButton() {
+    const submitButton = document.getElementById("chat-submit-button");
+    submitButton.addEventListener("click", () => {
+        const text = document.getElementById('chat-input').value;
+        console.log(text);
+        // TODO: add logic to send text to server
+    });
+}
